1505ac16221d437ff06a3032507c87e6cdf32d3b
Alexander Bigulov <42319615+bialger@users.noreply.github.com>
2024-03-20 01:19:18 +0300

    Update wanted.php

--- easymusic/wanted.php
+++ easymusic/wanted.php
@@ -1,7 +1,5 @@
 <?php
 	$wanted = array(
         array("bialger@mail.ru", "test", "bialger@mail.ru"),
-		array('nazarenckolizaveta@yandex.ru', 'help239', 'nazarenckolizaveta@yandex.ru'), //12 Apr 2022 Tue 15:31:29
-		array('afbut@bk.ru', 'Af28!!45', 'afbut@bk.ru'), //07 Aug 2022 Sun 10:26:20
 	);
 ?>
