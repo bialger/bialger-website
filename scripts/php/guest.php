2250f5b5e05cc0d1a48916c3d467f121e0e5003d
Alexander Bigulov <42319615+bialger@users.noreply.github.com>
2024-07-23 02:33:44 +0300

    Added correct login id cookie set

--- scripts/php/guest.php
+++ scripts/php/guest.php
@@ -1,6 +1,7 @@
 <?php
 	setcookie ("user-bialger", "guest", time()+(86400), "/");
 	setcookie ("passw-bialger", "password", time()+(86400), "/");
+	setcookie ("lid-bialger", hash("sha512", $_SERVER['HTTP_USER_AGENT'].$_SERVER['REMOTE_ADDR']), time()+(86400), "/");
 	if ((count($_GET) >= 1) and isset($_GET['retpath'])) {
 	    header('Location: https://bialger.com/'.substr($_GET['retpath'], 1));
 	}
