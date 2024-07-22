1236bdbd2ff6801bc19e874ac02988c0b13fe24c
Alexander Bigulov <42319615+bialger@users.noreply.github.com>
2024-07-23 02:35:42 +0300

    Rewriten testcode.php to make it more secure

--- scripts/php/testcode.php
+++ scripts/php/testcode.php
@@ -8,26 +8,47 @@
         if (($post == "") || (trim($post) == "")) $do = false;
     }
     $filename = "autogen_".substr($_COOKIE['user-bialger'], 23, 9).".php";
+    $gate = "<?php
+    include \"./hashes.php\";
+    include \"./hash_eq.php\";
+    include \"./checkAuth.php\";
+    checkAuth(0); 
+?>
+";
     if ((count($_POST) == 2) && $do) {
 	    $text = $_POST['code'];
 	    $file = fopen($filename, "w");
-	    fwrite($file, $text);
+	    fwrite($file, $gate.$text);
+	    fclose($file);
 	    header('Location: https://bialger.com/scripts/php/'.$filename);
     }
 ?>
 <html>
     <head>
-        <link rel="icon" sizes="32x32" type="image/png" href="https://yt3.ggpht.com/a/AATXAJwNdGwTvJkJt7H-vq8OP-zUQULu7co4wKhQlg=s900-c-k-c0xffffffff-no-rj-mo">
-        <title>Тест кода</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="/style/style.css">
+        <?php
+            include "./html_incl.php";
+            head("Тест кода", "style.css");
+        ?>
     </head>
     <body>
         <style>@import url(https://fonts.googleapis.com/css?family=Corbel);</style>
         <p class="bodytitle">Тест кода</p>
         <p>
             <form style="margin-left:3%;font-family:'Corbel' sans-serif;font-size:18px;" action="./testcode.php" method="post">
-                <textarea style="font-family:'Courier';font-size:18px;width:97%;height:50%;"name="code"><?php echo file_get_contents($filename); ?></textarea><br><br>
+                <textarea style="font-family:'Courier';font-size:18px;width:97%;height:50%;"name="code"><?php 
+                    if (file_exists($filename)) {
+                        $content = file_get_contents($filename);
+                        if (substr($content, 0, strlen($gate)) == $gate) {
+                            $content = substr($content, strlen($gate));
+                        }
+                        echo $content;
+                    }
+                    else {
+                        $file_tmp = fopen($filename, "w");
+                        fwrite($file_tmp, $gate);
+	                    fclose($file_tmp);
+                    }
+                ?></textarea><br><br>
                 <input style="font-family:'Corbel' sans-serif;font-size:18px;width:20%" type="submit" name="logon" value="Протестировать"/>
             </form>
 		</p>
