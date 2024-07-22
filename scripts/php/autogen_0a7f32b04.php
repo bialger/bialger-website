d9f03f02dd568535aa3af0c7dc4d887bf9727684
Alexander Bigulov <42319615+bialger@users.noreply.github.com>
2024-07-23 02:27:28 +0300

    Update autogen_0a7f32b04.php

--- scripts/php/autogen_0a7f32b04.php
+++ scripts/php/autogen_0a7f32b04.php
@@ -1,4 +1,12 @@
-<pre><?php
-print_r($_SERVER);
+<?php
+    include "./hashes.php";
+    include "./hash_eq.php";
+    include "./checkAuth.php";
+    checkAuth(0); 
 ?>
+<pre>
+<?php
+    print_r($_SERVER);
+?>                          
 </pre>
+                      
