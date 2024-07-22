64946f97b788566a073013cc5f570010365f72db
Alexander Bigulov <42319615+bialger@users.noreply.github.com>
2024-07-23 02:26:18 +0300

    Update download_current_app.php

--- easymusic/download_current_app.php
+++ easymusic/download_current_app.php
@@ -2,10 +2,10 @@
     if (isset($_GET['type'])) {
         $type = htmlentities($_GET['type']);
         if ($type == "Alpha") {
-            header('Location: https://disk.yandex.ru/d/gZX3vKW4ng6Lpg'); # 1.0.3
+            header('Location: https://disk.yandex.ru/d/n2TH7Px8KByRLg'); # 1.0.4
         }
         else if ($type == "Beta") {
-            header('Location: https://disk.yandex.ru/d/YRQOeu3gzP0Xpg'); # 0.9.8
+            header('Location: https://disk.yandex.ru/d/8T2nEOnSAal7VQ'); # 1.0.5
         }
         else {
             header('Location: https://disk.yandex.ru/d/T1p4FIzzbn_b0Q'); # 1.0.0
