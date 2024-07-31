6b54c293a18fd9fcceba02d82abb586da9b5a3f1
bialger <artur.bigulov@yandex.ru>
2024-08-01 02:31:28 +0300

    Updated EasyMusic links

--- easymusic/download_current_app.php
+++ easymusic/download_current_app.php
@@ -2,17 +2,17 @@
     if (isset($_GET['type'])) {
         $type = htmlentities($_GET['type']);
         if ($type == "Alpha") {
-            header('Location: https://github.com/bialger/circle21-music/releases/download/v1.0.5/EasyMusic-1.0.4-alpha.apk'); # 1.0.4
+            header('Location: https://github.com/bialger/EasyMusic/releases/download/v1.0.5/EasyMusic-1.0.4-alpha.apk'); # 1.0.4
         }
         else if ($type == "Beta") {
-            header('Location: https://github.com/bialger/circle21-music/releases/download/v1.0.5/EasyMusic-1.0.5-beta.apk'); # 1.0.5
+            header('Location: https://github.com/bialger/EasyMusic/releases/download/v1.0.5/EasyMusic-1.0.5-beta.apk'); # 1.0.5
         }
         else {
-            header('Location: https://github.com/bialger/circle21-music/releases/download/v1.0.5/EasyMusic-1.0-stable.apk'); # 1.0.0
+            header('Location: https://github.com/bialger/EasyMusic/releases/download/v1.0.5/EasyMusic-1.0-stable.apk'); # 1.0.0
         }
     }
     else {
-        header('Location: https://github.com/bialger/circle21-music/releases/download/v1.0.5/EasyMusic-1.0-stable.apk'); # 1.0.0
+        header('Location: https://github.com/bialger/EasyMusic/releases/download/v1.0.5/EasyMusic-1.0-stable.apk'); # 1.0.0
     }
     die();
 ?>
