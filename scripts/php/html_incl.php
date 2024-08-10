b517207a228aa7ffcab18422196ccff167757537
bialger <bigulov.sasha@gmail.com>
2024-08-10 03:32:55 +0300

    Added logo to assets

--- scripts/php/html_incl.php
+++ scripts/php/html_incl.php
@@ -1,6 +1,6 @@
 <?php
     function head ($header, $style="style.css") {
-        $msg = '<link rel="icon" size="32x32" type="image/png" href="https://yt3.ggpht.com/a/AATXAJwNdGwTvJkJt7H-vq8OP-zUQULu7co4wKhQlg=s900-c-k-c0xffffffff-no-rj-mo">
+        $msg = '<link rel="icon" size="32x32" type="image/png" href="https://bialger.com/assets/logo.png">
         <title>'.$header.'</title>
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css" href="/style/'.$style.'">'."\n";
